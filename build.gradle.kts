b06f953c7f42debf5551f918e4fd22ec8d3409b0
Elizabeth <foxcapade@gmail.com>
2022-04-19 09:25:10 -0400

    add test project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,21 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm") version "1.6.20"
+  id("org.jetbrains.dokka") version "1.6.10"
+  `java-library`
+  `maven-publish`
 }
 
 group = "org.veupathdb.lib.s3"
-version = "0.1.0"
+version = "0.1.0-SNAPSHOT"
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+
+  withSourcesJar()
+  withJavadocJar()
+}
 
 repositories {
   mavenCentral()
@@ -23,6 +34,18 @@ dependencies {
   testImplementation(kotlin("test"))
 }
 
+tasks.dokkaHtml {
+  outputDirectory.set(file("docs/dokka"))
+}
+
+tasks.dokkaJavadoc {
+  outputDirectory.set(file("docs/javadoc"))
+}
+
+task("docs") {
+  dependsOn("dokkaHtml", "dokkaJavadoc")
+}
+
 tasks.test {
   useJUnitPlatform()
 }
@@ -30,5 +53,43 @@ tasks.test {
 tasks.withType<KotlinCompile>().all {
   kotlinOptions {
     jvmTarget = "1.8"
+  }
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "GitHub"
+      url = uri("https://maven.pkg.github.com/VEuPathDB/lib-s34k")
+      credentials {
+        username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+        password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+      }
+    }
+  }
+
+  publications {
+    create<MavenPublication>("gpr") {
+      from(components["java"])
+      pom {
+        name.set("Generalized S3 API")
+        description.set("Provides a standard API for S3 operations which may be backed by varying implementations.")
+        url.set("https://github.com/VEuPathDB/lib-s34k")
+        developers {
+          developer {
+            id.set("epharper")
+            name.set("Elizabeth Paige Harper")
+            email.set("epharper@upenn.edu")
+            url.set("https://github.com/foxcapades")
+            organization.set("VEuPathDB")
+          }
+        }
+        scm {
+          connection.set("scm:git:git://github.com/VEuPathDB/lib-s34k.git")
+          developerConnection.set("scm:git:ssh://github.com/VEuPathDB/lib-s34k.git")
+          url.set("https://github.com/VEuPathDB/lib-s34k")
+        }
+      }
+    }
   }
 }
