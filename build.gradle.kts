a3121bc9121b23d7721fea7057bcf8463ab79cbf
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2023-11-06 12:17:28 -0500

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.8.22"
-  id("org.jetbrains.dokka") version "1.8.20"
+  kotlin("jvm") version "1.9.20"
+  id("org.jetbrains.dokka") version "1.9.10"
   `java-library`
   `maven-publish`
 }
 
 group = "org.veupathdb.lib.s3"
-version = "0.6.0+s34k-0.10.0"
+version = "0.6.0+s34k-0.10.1"
 
 java {
   sourceCompatibility = JavaVersion.VERSION_1_8
@@ -36,9 +36,9 @@ dependencies {
   implementation(kotlin("stdlib"))
   implementation(kotlin("stdlib-jdk8"))
   implementation("org.slf4j:slf4j-api:1.7.36")
-  implementation("io.minio:minio:8.5.3")
+  implementation("io.minio:minio:8.5.6")
 
-  api("org.veupathdb.lib.s3:s34k-core:0.9.0+s34k-0.10.0")
+  api("org.veupathdb.lib.s3:s34k-core:0.9.0+s34k-0.10.1")
 
   testImplementation(kotlin("test"))
 }
